3c30b732f15387b1cdd2a09ceeb06b40010b3104
Ardiegon <oskbsz@gmail.com>
2024-05-03 20:32:18 +0200

    amalgation init

--- src/agents/AMALGATION.py
+++ src/agents/AMALGATION.py
@@ -1,45 +1,29 @@
 import numpy as np
 from dataclasses import dataclass
 
+import utils.algorithmic as alg 
+from agents.base import BaseAgent, BaseAgentState
 from configs.AMALGATIONconfig import default_config
 from utils.misc import print_clean
 
 @dataclass
-class StateAMALGATION:
-    timestep: int
-    population: np.ndarray
-    # pewnie jeszcze coś
+class StateAMALGATION(BaseAgentState):
+    pass
 
-class AMALGATION:
+class AMALGATION(BaseAgent):
     def __init__(self, objective, population=None, config = {}) -> None:
-        self.config = default_config() | config
-        self.objective = objective
-        population = population if population is not None else self._init_population()
-        self.history = [self._init_state(population)]
-
-    def _init_population(self):
-        n_dims = self.config["pop_size"]
-        n_samples = self.config["pop_dims"]
-        mu = self.config["new_population_mean"]
-        sigma = self.config["new_population_variance"]
-        population = np.random.normal(mu, sigma, (n_dims, n_samples))
-        q_population = self._eval(population)
-        sorted_args = np.argsort(q_population)
-        return population[sorted_args]
+        super().__init__(objective, population, config = default_config() | config)
     
     def _init_state(self, population):
+        # Do zrobienia
         return StateAMALGATION(0, population)
 
     def _make_step(self, state):
+        # Do zrobienia
         pass
-        # Do uzupełnienia
-
-    def _eval(self, pop):
-        if len(pop.shape) == 1: # single specimen
-            pop = pop[None,:]
-        return self.objective(pop)
     
     def run(self):
+        # Do zrobienia
         # pewnie mało się zmieni
         max_iters = self.config["max_iters"]
         state = self.history[-1]
@@ -52,15 +36,4 @@ class AMALGATION:
                 print_clean(f"Timestep: {state.timestep}\nCurrent Mean: {np.mean(state.population, axis=0)}\nEval: {self._eval(np.mean(state.population, axis=0))}")
         self.dump_history_to_file(f"src/checkpoints/lastamalgation.npy")
 
-    def get_history_means(self):
-        means = []
-        for state in self.history:
-            means.append(np.mean(state.population, axis=0))
-        return np.array(means)
-    
-    def dump_history_to_file(self, file_path):
-        np.save(file_path, np.array(self.history))
-
-    def load_history_from_file(self, file_path):
-        self.history = np.load(file_path, allow_pickle=True)
     
--- src/configs/AMALGATIONconfig.py
+++ src/configs/AMALGATIONconfig.py
@@ -1,7 +1,10 @@
 def default_config(pop_dims=2):
+    pop_size = pop_dims*5
     return {
+    "pop_size": pop_size,
+    "pop_dims": pop_dims,
     #whatever goes here
-
+    #
     # init population params 
     "new_population_mean": 5.0,
     "new_population_variance": 3.0,
